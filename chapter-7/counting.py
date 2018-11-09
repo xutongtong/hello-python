2056e9920100bb42985ecd132a75ebfef0ec2732
xutongtong <xu.tongtong@hbtown.com>
2018-11-09 14:51:25 +0800

    Python从入门到实践-第七章练习

---
+++ chapter-7/counting.py
@@ -0,0 +1,8 @@
+current_number = 1
+while current_number <= 5:
+    current_number += 1
+
+    if current_number % 2 == 0:
+        continue
+
+    print(current_number)
---
+++ chapter-7/rollercoaster.py
@@ -0,0 +1,7 @@
+height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >= 36:
+    print("\n You're tall enough to ride!")
+else:
+    print("\n You'll be able to ride when you're a little older.")
