49345330d8569ae70d1e795712cbf04e13f10153
xutongtong <xu.tongtong@hbtown.com>
2018-11-09 11:46:48 +0800

    Python从入门到实践-第三章练习

---
+++ chapter-3/cars.py
@@ -0,0 +1,14 @@
+cars = ['bmw', 'audi', 'toyata', 'subaru']
+# cars.sort()
+# print(cars)
+
+# cars.sort(reverse=True)
+# print(cars)
+
+print(cars)
+
+# print(sorted(cars))
+cars.reverse()
+print(cars)
+print(len(cars))
+print(cars[5])
---
+++ chapter-3/motorcycles.py
@@ -0,0 +1,50 @@
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+#
+# motorcycles[0] = 'ducati'
+# print(motorcycles)
+#
+# motorcycles.append('ducati')
+# print(motorcycles)
+
+# motorcycles = []
+# motorcycles.append('honda')
+# motorcycles.append('yamaha')
+# motorcycles.append('suzuki')
+#
+# print(motorcycles)
+#
+# motorcycles.insert(0, 'ducati')
+# print(motorcycles)
+#
+# del motorcycles[0]
+# print(motorcycles)
+#
+# del motorcycles[1]
+# print(motorcycles)
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+#
+# poped_motorcycle = motorcycles.pop()
+# print(motorcycles)
+# print(poped_motorcycle)
+#
+# poped_motorcycle = motorcycles.pop(1)
+# print(motorcycles)
+# print(poped_motorcycle)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+motorcycles.remove('ducati')
+print(motorcycles)
+
+
+
+
+
+
+
+
+
